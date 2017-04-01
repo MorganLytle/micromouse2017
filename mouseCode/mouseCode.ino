06d83059280638576a7d0284d38eeb75f7c9feaa
Morgan Lytle <Morgan Lytle>
2017-04-01 00:13:54 -0700

    update 3

--- mouseCode/mouseCode.ino
+++ mouseCode/mouseCode.ino
@@ -1,8 +1,33 @@
-//hello
-//void start()
-//{
-//  
-//}
+
+
+int m1Enable = 2;
+int m1Logic1 = 3;
+int m1Logic2 = 4;
+int m2Enable = 1;
+int m2Logic1 = 5;
+int m2Logic2 = 6;
+int leftSensor = A2;
+int middleSensor = A1;
+int rightSensor = A0;
+int baseReading = 40;//Callibrate
+int encoder1 = ;//FIXME
+int encoder2 = ;//FIXME
+
+
+void calibrate(){
+ baseReading = digitalRead(leftSensor);
+}
+void start()
+{
+  int offset = 50;
+  if(analogRead(middleSensor) > baseReading+offset)
+  {
+    forward();
+  }
+  else{
+    halt();
+  }
+}
 //void control() //PID control
 //{
 //  
@@ -13,44 +38,44 @@
 //}
 
 
-void forward_accelerate()
+void forward( )
 {
+  int i = 150;
   digitalWrite(m1Enable, HIGH); //m1 is left motor, m2 is right motor
   digitalWrite(m2Enable, HIGH);
-  analogWrite(m1Logic1, LOW);
-  analogWrite(m2Logic1, LOW); 
-  for(int i = 0; i <= 255; ++i)
-  {
-    analogWrite(m1Logic2, i);
-    analogWrite(m2Logic2, i);
+  digitalWrite(m1Logic1, LOW);
+  digitalWrite(m2Logic1, LOW); 
+  analogWrite(m1Logic2, i);
+  analogWrite(m2Logic2, i);
   }
   
 }
-//void left()
-//{
-//  digitalWrite(m1Enable, LOW);
-//  digitalWrite(m2Enable, HIGH);
-//  digitalWrite(m2Logic1, HIGH);
-//  digitalWrite(m2Logic2, LOW);
-//}
-//
-//void left_accelerate()
+void left()
+{
+  digitalWrite(m1Enable, HIGH);
+  digitalWrite(m2Enable, HIGH);
+  digitalWrite(m2Logic1, LOW);
+  digitalWrite(m2Logic2, HIGH);
+  digitalWrite(m1Logic1, HIGH);
+  digitalWrite(m1Logic2, LOW);
+}
+
+void right()
+{
+  digitalWrite(m1Enable, HIGH);
+  digitalWrite(m2Enable, HIGH);
+  digitalWrite(m1Logic1, LOW);
+  digitalWrite(m1Logic2, HIGH);
+  digitalWrite(m2Logic1, HIGH);
+  digitalWrite(m2Logic2, LOW);
+  
+}
+
+//void uTurn()
 //{
 //  
 //}
-//
-//void right()
-//{
-//  digitalWrite(m1Enable, HIGH);
-//  digitalWrite(m2Enable, LOW);
-//  digitalWrite(m1Logic1, HIGH);
-//  digitalWrite(m1Logic2, LOW);
-//}
-//
-//void left_accelerate()
-//{
-//  
-//}
+
 
 void halt()
 {
@@ -58,20 +83,12 @@ void halt()
   digitalWrite(m2Enable, LOW);
 }
 
-int m1Enable = 7;
-int m1Logic1 = 6;
-int m1Logic2 = 5;
-int m2Enable = 4;
-int m2Logic1 = 11;
-int m2Logic2 = 10;
-//int leftSensor = ;
-int middleSensor = ;
-//int rightSensor = ;
-//int encoder = ;
+
 
 
 void setup()
 {
+  Serial.begin(9600);
   pinMode(m1Enable, OUTPUT);
   pinMode(m2Enable, OUTPUT);
   pinMode(m1Logic1, OUTPUT);
@@ -82,10 +99,12 @@ void setup()
   pinMode(middleSensor, INPUT);
   pinMode(rightSensor, INPUT);
   pinMode(encoder, INPUT);
+  calibrate();
   
 }
 
 void loop() {
+//55555555555555555555555555555555555
 
-
+}
 }
