e23607735714084346decc9a501517fa93f3e60b
Morgan Lytle <Morgan Lytle>
2017-04-08 12:19:53 -0700

    update 5

--- mouseCode/mouseCode.ino
+++ mouseCode/mouseCode.ino
@@ -12,13 +12,16 @@ int middleSensor = A1;
 int rightSensor = A0;
 int baseReadingWall = 40;//Callibrated value for wall reading
 int baseReading = 40; //Callibrated value for no wall
-int encoder1 = ;//FIXME
-int encoder2 = ;//FIXME
+//int encoder1 = ;//FIXME
+//int encoder2 = ;//FIXME
 int motorStatus = 0; //0=halt/1=foward/2=left/3=right
 int leftVal = 0;
 int midVal = 0;
 int rightVal = 0;
-<int> array baseMaze[16][16] = { //distances to center with no walls
+int wallCase = 0;
+int deadEndFlag = 0;
+int offset = 0;
+int baseMaze[16][16] = { //distances to center with no walls
   {14, 13, 12, 11, 10, 9, 8, 7, 7, 8, 9, 10, 11, 12, 13, 14},
   {13, 12, 11, 10, 9, 8, 7, 6, 6, 7, 8, 9, 10, 11, 12, 13},
   {12, 11, 10, 9, 8, 7, 6, 5, 5, 6, 7, 8, 9, 10, 11, 12},
@@ -37,6 +40,14 @@ int rightVal = 0;
   {14, 13, 12, 11, 10, 9, 8, 7, 7, 8, 9, 10, 11, 12, 13, 14}
 };
 
+
+
+void halt()
+{
+  motorStatus = 0;
+  digitalWrite(m1Enable, LOW);
+  digitalWrite(m2Enable, LOW);
+}
   
 void calibrate(){ //Gets readings that will be used to compare to future readings
  baseReadingWall = analogRead(leftSensor); //Used to infer information about its environment
@@ -61,15 +72,17 @@ void forward( )
 {
   int motorValue = 150;
   motorStatus = 1;
+
   digitalWrite(m1Enable, HIGH); //m1 is left motor, m2 is right motor
   digitalWrite(m2Enable, HIGH);
   digitalWrite(m1Logic1, LOW);
   digitalWrite(m2Logic1, LOW); 
   analogWrite(m1Logic2, motorValue);
   analogWrite(m2Logic2, motorValue);
+  
   }
   
-}
+
 void left()
 {
   motorStatus = 2;
@@ -99,12 +112,6 @@ void right()
 //}
 
 
-void halt()
-{
-  motorStatus = 0;
-  digitalWrite(m1Enable, LOW);
-  digitalWrite(m2Enable, LOW);
-}
 
 
 
@@ -121,7 +128,8 @@ void setup()
   pinMode(leftSensor, INPUT);
   pinMode(middleSensor, INPUT);
   pinMode(rightSensor, INPUT);
-  pinMode(encoder, INPUT);
+ // pinMode(encoder1, INPUT);
+ // pinMode(encoder2, INPUT);
   calibrate();
   start();
   
@@ -136,12 +144,60 @@ leftVal = analogRead(leftSensor); //Read Peripherals
 midVal = analogRead(middleSensor);
 rightVal = analogRead(rightSensor);
 
-
-
-
 //
 //
 //Determine walls and choose action
+if(deadEndFlag == 0)
+{
+if(midVal < baseReadingWall+offset)
+{
+  forward();
+}
+else{
+  halt();
+if((leftVal >= baseReadingWall+offset)&&(midVal >= baseReadingWall+offset) && (rightVal <= baseReadingWall+offset))//right turn detected
+{
+  wallCase = 1;
+  right();
+}
+else if((leftVal <= baseReadingWall+offset)&&(midVal >= baseReadingWall+offset) && (rightVal >= baseReadingWall+offset))//left turn detected
+{
+  wallCase = 2;
+  left();
+}
+else if((leftVal <= baseReadingWall+offset)&&(midVal >= baseReadingWall+offset) && (rightVal <= baseReadingWall+offset))//possible left or right detected
+{
+  wallCase = 3;
+  
+}
+else if((leftVal <= baseReadingWall+offset)&&(midVal >= baseReadingWall+offset) && (rightVal >= baseReadingWall+offset))//dead end detected
+{
+  wallCase = 4;
+ // uTurn();
+  deadEndFlag = 1;
+}
+}
+}
+else{
+  //look for first turn to break loop
+  forward();
+  if(leftVal <= baseReadingWall+offset)
+  {
+    left();
+    deadEndFlag = 0;
+  }  
+  else if(rightVal <= baseReadingWall+offset)
+  {
+    right();
+    deadEndFlag = 0;
+  } 
+  else
+  {
+    deadEndFlag = 1;
+  }
+}
+
+
 //
 //
 
